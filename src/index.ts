5ec5eb3f64a93d5113913aa076a91fa810af3864
Eva Tavares <eva.programadora@gmail.com>
2023-06-08 22:02:21 -0300

    Novas Rotas

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,23 @@
 import express from "express";
+import { UserMiddleware } from "./middleware/user.middleware";
+import { ErrandController } from "./controllers/errands.controller";
+import { UserController } from "./controllers/user.controller";
 
 const app = express();
 app.use(express.json());
+
+//List Users
+app.get("/users", new UserController().getAllUsers);
+
+// Listar por id
+app.get("/users/:id", new UserController().listUserId);
+
+//Create Errand
+app.post(
+  "/user/:userId/errands",
+  [UserMiddleware.validateUser],
+  new ErrandController().createErrands
+);
 
 app.listen(3333, () => {
   console.log("API is running");
