8279db572fa802943f0c402e5ec2e053e3112900
Eva Tavares <eva.programadora@gmail.com>
2023-06-08 22:03:11 -0300

    Dados Mocados

---
+++ src/database/errands.ts
@@ -0,0 +1,9 @@
+import { Errand } from "../models/errand";
+
+export const errands = [
+  new Errand("compras", "feira do mês"),
+  new Errand("férias", "viagem para paris"),
+  new Errand("Atividade", "finalizar API"),
+  new Errand("estágio", "procurar vagas"),
+  new Errand("lazer", "sextou"),
+];
