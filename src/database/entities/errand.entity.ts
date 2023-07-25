7e61ff397553090579759fe420de71fc8730898c
Eva Tavares <eva.programadora@gmail.com>
2023-07-25 16:08:09 -0300

    criação das Entities

---
+++ src/database/entities/errand.entity.ts
@@ -0,0 +1,28 @@
+import { Column, Entity, PrimaryColumn } from "typeorm";
+import { StatusErrand } from "../../models/errand";
+
+@Entity("errand")
+export class ErrandEntity {
+  @PrimaryColumn()
+  id: string;
+
+  @Column()
+  title: string;
+
+  @Column()
+  description: string;
+
+  @Column({
+    enum: StatusErrand,
+  })
+  type: string;
+
+  @Column({ name: "id_user" })
+  idUser: string;
+
+  // @Column({ name: "dthr_criacao" })
+  // dthrCriacao: Data;
+
+  // @Column({ name: "dthr_atualizacao" })
+  // dthrAtualizacao: Data;
+}
---
+++ src/database/entities/user.entity.ts
@@ -0,0 +1,19 @@
+import { Column, Entity, PrimaryColumn } from "typeorm";
+
+@Entity("user")
+export class UserEntity {
+  @PrimaryColumn()
+  id: string;
+
+  @Column()
+  name: string;
+
+  @Column()
+  email: string;
+
+  @Column()
+  password: string;
+
+  @Column({ name: "dthr_criacao" })
+  dthrCriacao: Date;
+}
