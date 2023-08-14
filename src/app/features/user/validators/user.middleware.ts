f09597900a1af1f6bbac4b96157096594af8fcc5
Eva Tavares <eva.programadora@gmail.com>
2023-08-14 16:52:11 -0300

    refactor: remoção do diretório data

--- src/app/features/user/validators/user.middleware.ts
+++ src/app/features/user/validators/user.middleware.ts
@@ -1,7 +1,6 @@
 import { NextFunction, Request, Response } from "express";
 import { StatusCodes } from "http-status-codes";
 import { ApiResponse } from "../../../shared/util/Api.response.adapter";
-import { usersDb } from "../../../../data/users";
 
 export class UserMiddleware {
   public static validateUser(req: Request, res: Response, next: NextFunction) {
--- src/data/errands.ts
+++
@@ -1,9 +0,0 @@
-import { Errand, StatusErrand } from "../app/models/errand";
-
-export const errands = [
-  // new Errand("compras", "feira do mês", StatusErrand.ARCHIVED),
-  // new Errand("férias", "viagem para paris", StatusErrand.UNARCHIVED),
-  // new Errand("Atividade", "finalizar API", StatusErrand.ARCHIVED),
-  // new Errand("estágio", "procurar vagas", StatusErrand.UNARCHIVED),
-  // new Errand("lazer", "sextou", StatusErrand.ARCHIVED),
-];
