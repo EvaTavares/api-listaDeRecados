de376b5809827ec04fe17bb31eb935dd20dd4fae
Eva Tavares <eva.programadora@gmail.com>
2023-08-17 17:15:46 -0300

    feat: update usecase de errand

---
+++ src/app/features/errand/usecases/update-errand.usecase.ts
@@ -0,0 +1,57 @@
+import { Result } from "../../../shared/contracts/result.contract";
+import { Return } from "../../../shared/util/return.adapter";
+import { UserRepository } from "../../user/repositories/user.repository";
+import { ErrandRepository } from "../repositories/errand.repository";
+
+interface UpdateErrandParams {
+  userId: string;
+  idErrand: string;
+  title: string;
+  description: string;
+}
+
+export class UpdateErrandUsecase {
+  /**
+   * executa o usecase de atualização do errand
+   * @param params ID do user e do errand + valores a serem atualizados
+   * @returns lista de errands atualizada
+   */
+  public async execute(params: UpdateErrandParams): Promise<Result> {
+    // verifica se o usuário existe
+    const user = await new UserRepository().getById(params.userId);
+
+    if (!user) {
+      return Return.notFound("User");
+    }
+
+    //  Busca o recado no BD
+    const errandRepository = new ErrandRepository();
+    // tem que ser do tipo errand | undefined
+    const errand = await errandRepository.getByIdErrand(params.idErrand);
+
+    if (!errand) {
+      return Return.notFound("Errand");
+    }
+
+    // Apenas atualiza os dados se forem informados nos params
+    if (params.title) {
+      errand.title = params.title;
+    }
+
+    if (params.description) {
+      errand.description = params.description;
+    }
+
+    // salvar...
+    await errandRepository.update(errand);
+
+    const errands = await errandRepository.list({
+      userId: params.userId,
+    });
+
+    return Return.success(
+      "Errand successfully updated",
+      errands.map((errand) => errand.toJson())
+    );
+  }
+}
