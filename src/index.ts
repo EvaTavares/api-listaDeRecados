2658c1ede3d61079e8a57404cfca4ef9191f8594
Eva Tavares <eva.programadora@gmail.com>
2023-08-20 23:29:41 -0300

    feat: Redis config

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,10 @@
 import { Database } from "./main/database/database.connection";
-import { Server } from "./main/server/express.server";
+import { CacheDatabase } from "./main/database/redis.connection";
+import { Server } from "./main/config/express.config";
 import "reflect-metadata";
 
 // inicializar o banco de dados, antes do listen
-Promise.all([Database.connect()]).then(() => {
-  console.log("Database is connected");
-  Server.listen();
+Promise.all([Database.connect(), CacheDatabase.connect()]).then(() => {
+  const app = Server.create();
+  Server.listen(app);
 });
