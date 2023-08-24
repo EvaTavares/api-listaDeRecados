426a21b0bf068fd9a4b317db1f57d0067f90f424
Eva Tavares <eva.programadora@gmail.com>
2023-08-24 15:37:11 -0300

    feat: index adapter e result adapter

---
+++ src/app/shared/util/index.ts
@@ -0,0 +1,4 @@
+export * from "./Api.response.adapter";
+export * from "./result.contract";
+export * from "./usecase.contract";
+export * from "./response.adapter";
---
+++ src/app/shared/util/response.adapter.ts
@@ -0,0 +1,69 @@
+import { Result } from "../contracts/result.contract";
+
+export class UsecaseResponse {
+  public static success(message: string, data: any): Result {
+    return {
+      ok: true,
+      message,
+      data,
+      code: 200,
+    };
+  }
+
+  public static created(message: string, data: any): Result {
+    return {
+      ok: true,
+      message,
+      data,
+      code: 201,
+    };
+  }
+
+  public static fieldNotProvided(field: string): Result {
+    return {
+      ok: false,
+      message: field + "was not provided",
+      code: 400,
+    };
+  }
+
+  public static notFound(entity: string): Result {
+    return {
+      ok: false,
+      message: `${entity} not found`,
+      code: 404,
+    };
+  }
+
+  public static invalid(field: string): Result {
+    return {
+      ok: false,
+      message: field + " is invalid",
+      code: 400,
+    };
+  }
+
+  public static invalidCredentials(): Result {
+    return {
+      ok: false,
+      message: "Unauthorized access",
+      code: 401,
+    };
+  }
+
+  public static invalidField(field: string, reason: string): Result {
+    return {
+      ok: false,
+      message: `${field} is invalid: ${reason} `,
+      code: 400,
+    };
+  }
+
+  public static genericError(error: any): Result {
+    return {
+      ok: false,
+      message: error.toString(),
+      code: 500,
+    };
+  }
+}
